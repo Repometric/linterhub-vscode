38b8259708deb7b0f9d63e85afd62b9c507b0de3
Oleg Safonov <by.xsofa@gmail.com>
2016-10-21 13:38:43 +0300

    Improve integration with parser, some fixes with parser's path in server.ts

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -5,36 +5,51 @@ import * as vscode from 'vscode';
 import { workspace, Disposable, ExtensionContext } from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind } from 'vscode-languageclient';
 var fs = require('fs');
+import { spawn, execSync, exec } from "child_process";
 
-let linters: string[] = ["jshint", "csslint", "coffeelint"];
+let linters: string[] = [];
 let appPath: string = path.resolve(__dirname);
 let appRoot: string = appPath.substr(0, appPath.length - 7);
+let cli_path: string = "dotnet " + appRoot + "repometric/Integrations.Linters/src/Metrics.Integrations.Linters.Cli/bin/publish/Metrics.Integrations.Linters.Cli.dll";
+let config_path: string = appRoot + "repometric/Integrations.Linters/src/Metrics.Integrations.Linters.Cli/config.Windows.json";
+
+function status(s: string): void
+{
+	vscode.window.setStatusBarMessage("LinterHub: " + s);
+}
 
 export function activate(context: ExtensionContext) {
-	vscode.window.setStatusBarMessage("LinterHub: ready");
 	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
 	let debugOptions = { execArgv: ["--nolazy", "--debug=6004"] };
-	
 	// If the extension is launched in debug mode then the debug server options are used
 	// Otherwise the run options are used
 	let serverOptions: ServerOptions = {
 		run : { module: serverModule, transport: TransportKind.ipc },
 		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
 	}
-	
+
 	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
-		documentSelector: ['javascript', 'css', 'coffeescript'],
+		documentSelector: [],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contain in the workspace
 			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
 		}
 	}
+
+	let stdout = execSync(cli_path + " --mode Linters --config " + config_path).toString();
+	let parsed = JSON.parse(stdout);
+	let ds: string[] = [];
+	parsed.forEach(element => {
+		if(ds.findIndex(x => x == element.languages) == -1) ds.push(element.languages);
+		linters.push(element.name);
+	});
+	clientOptions.documentSelector = ds;
 	
 	// Create the language client and start the client.
 	let lc = new LanguageClient('LinterHub Server', serverOptions, clientOptions);
 	lc.onRequest({method: "SetBarMessage"}, (params : { Message : string }) : void => {
-		vscode.window.setStatusBarMessage("LinterHub: " + params.Message);
+		status(params.Message);
 	});
 	let disposable = lc.start();
 	// Push the disposable to the context's subscriptions so that the 
@@ -44,7 +59,7 @@ export function activate(context: ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('linterhub.activate', () => {
 		vscode.window.showQuickPick(linters).then(function(value: string){
 			let linter: string = value.charAt(0).toUpperCase() + value.slice(1);
-			let fpath: string = workspace.rootPath + "/.linterhub/" + value + ".json";
+			let fpath: string = workspace.rootPath + "/.linterhub/" + value + "/config.json";
 			if (fs.existsSync(fpath)) {
 				vscode.window.showInformationMessage(linter + " is already initialized! Open .linterhub/" + value + ".json to edit settings");
 			}
@@ -58,7 +73,7 @@ export function activate(context: ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('linterhub.deactivate', () => {
 		vscode.window.showQuickPick(linters).then(function(value: string){
 			let linter: string = value.charAt(0).toUpperCase() + value.slice(1);
-			let fpath: string = workspace.rootPath + "/.linterhub/" + value + ".json";
+			let fpath: string = workspace.rootPath + "/.linterhub/" + value + "/config.json";
 			if (!fs.existsSync(fpath)) {
 				vscode.window.showInformationMessage(linter + " is not initialized");
 			}
@@ -73,4 +88,5 @@ export function activate(context: ExtensionContext) {
 			}
 		});
 	}));
+	status("ready");
 }
