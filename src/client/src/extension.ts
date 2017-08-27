bdf321ec21de30fc6c0dc9c720137fddd68cc723
Oleg Safonov <by.xsofa@gmail.com>
2017-08-27 23:53:37 +0300

    Update vscode extension for linterhub cli 0.6.0

--- src/client/src/extension.ts
+++ src/client/src/extension.ts
@@ -1,69 +1,56 @@
 'use strict';
 
 import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
-import { window, commands } from 'vscode';
+import { ExtensionContext } from 'vscode';
+import { commands, window } from 'vscode';
 import { LanguageClient, LanguageClientOptions, TransportKind, ServerOptions } from 'vscode-languageclient';
-import { StatusNotification, ConfigRequest, UpdateConfigRequest } from 'linterhub-vscode-shared';
 import { Integration } from './integration';
-import { LinterhubTypes } from '@repometric/linterhub-ide';
 
 export function activate(context: ExtensionContext) {
 	let integration = new Integration();
-	integration.initialize().then(() => {
-		// Setup and start client
-		let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
-		let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
 
-		let serverOptions: ServerOptions = {
-			run : { module: serverModule, transport: TransportKind.ipc },
-			debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
-		};
+	let serverModule = context.asAbsolutePath(path.join('server', 'server.js'));
+	let debugOptions = { execArgv: ["--nolazy", "--debug=6010"] };
 
-		let clientOptions: LanguageClientOptions = {
-			documentSelector: integration.languages,
-			synchronize: {
-				configurationSection: 'linterhub'
-			}
-		};
+	let serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc },
+		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+	};
 
-		let client = new LanguageClient('Linterhub', serverOptions, clientOptions);
-		let disposable = client.start();
+	let clientOptions: LanguageClientOptions = {
+		initializationFailedHandler: (error) => {
+			client.error('Server initialization failed.', error);
+			client.outputChannel.show(true);
+			return false;
+		},
+		documentSelector: [{
+			pattern: "**/*.*",
+			scheme: "file"
+		}]
+	};
 
-		client.onRequest(ConfigRequest, () =>
-		{
-			const config = workspace.getConfiguration();
-			return {
-				proxy: config.get<string>('http.proxy'),
-				strictSSL: config.get('http.proxyStrictSSL', true)
-			};
+	let client = new LanguageClient('Linterhub', serverOptions, clientOptions);
+	let disposable = client.start();
+
+	return client.onReady().then(() => {
+		integration.setClient(client);
+		integration.setupUi();
+
+		client.sendRequest("linterhub/version").then((version: string) => {
+			client.info("CLI VERSION: " + version)
 		});
 
-		client.onRequest(UpdateConfigRequest, (params) =>
-		{
-			let config: any = workspace.getConfiguration('linterhub');
-            config.update('cliPath', params.cliPath, true);
-			config.update('mode', params.mode, true);
-			return true;
+		context.subscriptions.push(
+			commands.registerCommand('linterhub.activate', () => integration.activate(true)),
+			commands.registerCommand('linterhub.deactivate', () => integration.activate(false)),
+			commands.registerCommand('linterhub.showOutput', () => integration.showOutput()),
+			commands.registerCommand('linterhub.ignoreWarning', (params) => integration.ignoreWarning(params)),
+			integration.statusBarItem,
+			disposable
+		);
+	})
+		.catch((e) => {
+			window.showErrorMessage(e.toString())
 		});
-
-		return client.onReady().then(() => {
-			integration.setClient(client);
-			integration.setupUi();
-			context.subscriptions.push(
-				commands.registerCommand('linterhub.analyze', () => integration.analyze()),
-				commands.registerCommand('linterhub.analyzeFile', () => integration.analyzeFile(window.activeTextEditor.document.uri.toString())),
-				commands.registerCommand('linterhub.activate', () => integration.activate()),
-				commands.registerCommand('linterhub.deactivate', () => integration.deactivate()),
-				commands.registerCommand('linterhub.showOutput', () => integration.showOutput()),
-				commands.registerCommand('linterhub.ignoreWarning', (params: LinterhubTypes.IgnoreWarningParams) => integration.ignoreWarning(params)),
-				integration.statusBarItem,
-				disposable
-			);
-
-			// Setup events
-			client.onNotification(StatusNotification, (params) => integration.updateStatus(params));
-		});
-	});
 }
 
